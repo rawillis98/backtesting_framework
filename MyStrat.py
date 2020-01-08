b278436168b6c147b3cb3a71a36843dcadba35c2
Alex Willis <ricwillis98@gmail.com>
2020-01-07 22:36:08 -0600

    Changed params on MyStrat

--- MyStrat.py
+++ MyStrat.py
@@ -1,5 +1,7 @@
 from Strategy import Strategy
 from Backend_Alpaca import AlpacaBackend
+from Backend_Backtest import AlphaVantageBacktest 
+import datetime
 from datetime import timedelta, time
 import warnings
 import logging
@@ -8,13 +10,15 @@ warnings.filterwarnings("ignore")
 
 class MyStrat(Strategy):
     name = "My Strat"
-    backend = AlpacaBackend('alpaca.key', 'alpaca.secret')
+    # backend = AlpacaBackend('alpaca.key', 'alpaca.secret')
+    backend = AlphaVantageBacktest(start=datetime.datetime(2017, 1, 3, 9, 30),
+                                   end=datetime.datetime(2017, 6, 1))
     universe = ["FB", "AAPL", "AMZN", "NFLX", "GOOG"]
 
     def __init__(self):
         super(MyStrat, MyStrat).__init__(self)
         self.schedule_function("buy", trading_days=1, tod=time(11, 16))
-        self.schedule_function("sell", trading_days=1, tod=time(11, 17))
+        self.schedule_function("sell", trading_days=2, tod=time(11, 17))
         logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
         self.logger = logging.getLogger(self.name)
         self.logger.info("Initialized My Strat!")
@@ -30,5 +34,7 @@ class MyStrat(Strategy):
         for symbol in self.universe:
             self.backend.order(symbol, -self.context['qty'])
 
-s = MyStrat()
-s.run()
+if __name__ == '__main__':
+    s = MyStrat()
+    s.run()
+    s.analyze()
